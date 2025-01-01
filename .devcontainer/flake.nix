446d42d05430562ccbf3804ed071f368e7a961f6
Swelio <24651896+Swelio@users.noreply.github.com>
2025-01-01 21:05:46 +0100

    feat(wip): generate image from nix

--- .devcontainer/flake.nix
+++ .devcontainer/flake.nix
@@ -14,6 +14,51 @@
       let pkgs = import nixpkgs { inherit system; };
       in {
         formatter = pkgs.nixfmt;
-        devShells.default = import ./shell.nix { inherit nixpkgs system; };
+        packages = {
+          default = import ./shell.nix { inherit nixpkgs system; };
+          image = pkgs.dockerTools.buildLayeredImage {
+            name = "devcontainer";
+            tag = "dev";
+            fromImage = pkgs.dockerTools.pullImage {
+              imageName = "docker.io/debian";
+              imageDigest =
+                "sha256:d365f4920711a9074c4bcd178e8f457ee59250426441ab2a5f8106ed8fe948eb";
+              sha256 = "sha256-XTOxjlTJ1ov8a0hsxVqhkKyny+rRVph3L12JSJrSSiQ=";
+            };
+            config = {
+              Cmd = [ "${pkgs.nushell}/bin/nu" ];
+              User = "dev";
+            };
+            fakeRootCommands = ''
+              #!${pkgs.bash}
+              set -euo pipefail
+
+              for file in /usr/bin/*; do
+                ln -s $file "/bin/$(/bin/basename $file)"
+              done
+
+              for file in /usr/lib/*; do
+                ln -s $file "/lib/$(/bin/basename $file)"
+              done
+
+              ${pkgs.dockerTools.shadowSetup}
+              useradd --create-home --user-group --shell ${pkgs.nushell}/bin/nu dev
+            '';
+            enableFakechroot = true;
+            contents = with pkgs; [
+              bashInteractive
+              bat
+              bat-extras.batman
+              diff-so-fancy
+              fd
+              ripgrep
+              nushell
+              starship
+              vim
+              zsh
+              zsh-completions
+            ];
+          };
+        };
       });
 }
--- .devcontainer/shell.nix
+++ .devcontainer/shell.nix
@@ -1,3 +1,14 @@
 { nixpkgs, system }:
 let pkgs = import nixpkgs { inherit system; };
-in pkgs.mkShell { buildInputs = with pkgs; [ bat bat-extras.batman diff-so-fancy fd ripgrep nushell starship vim ]; }
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    bat
+    bat-extras.batman
+    diff-so-fancy
+    fd
+    ripgrep
+    nushell
+    starship
+    vim
+  ];
+}
